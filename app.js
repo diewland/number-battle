f8d40386e51be55cc8207a7099c6f3c88f66eb36
yuwat <yuwat@mfec.co.th>
2021-03-11 23:30:28 +0700

    dev dev

---
+++ app.js
@@ -0,0 +1,44 @@
+const PLAYER_INFO = {
+  1: {
+    title: "ผู้เล่นฝ่ายแดง",
+    color_clz: "danger",
+  }
+  2: {
+    title: "ผู้เล่นฝ่ายน้ำเงิน",
+    color_clz: "info",
+  }
+}
+
+function render_player_table(no) {
+  /*
+  <table class='table table-danger table-striped'>
+    <thead>
+      <tr class='text-center'>
+        <th colspan='3'>ผู้เล่นคนที่ 1</th>
+      </tr>
+      <tr>
+        <th>ตัวเลข</th>
+        <th>ถูก</th>
+        <th>ตรง</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>****</td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>****</td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>x</td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+  */
+}
