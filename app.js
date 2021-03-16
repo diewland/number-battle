e0cf191158f2015ff1d42dcd33f8eb3467de01d2
yuwat <yuwat@mfec.co.th>
2021-03-16 22:52:54 +0700

    engine done

--- app.js
+++ app.js
@@ -2,8 +2,14 @@
 // CONSTANT
 //
 
-const PLAYER_1 = 'player_1'
-const PLAYER_2 = 'player_2'
+const PLAYER_1 = true;
+const PLAYER_2 = false;
+
+const PATT_INPUT = /^\d{4}$/;
+
+const HINT_FULL = "O"; //"<img height='24' src='./img/heart_full.png'> ";
+const HINT_HALF = "X"; //"<img height='24' src='./img/heart_half.png'> ";
+const HINT_NONE = "-"; //"<img height='24' src='./img/heart_blank.png'> ";
 
 //
 // STATE
@@ -11,23 +17,61 @@ const PLAYER_2 = 'player_2'
 
 let p1_won = 0;
 let p2_won = 0;
+let p1_ans = "1234"; // TODO
+let p2_ans = "5678"; // TODO
+let p1_num = [];
+let p2_num = [];
+let input_num = "";
+let next_turn = PLAYER_1;
 
 //
 // FLOW
 //
 
-// TODO
+function engine() {
+  // enter answers
+  while (!p1_ans.match(PATT_INPUT)) { p1_ans = prompt("*** SECRET ***\nEnter Player 1 number (4 digits)") || ""; }
+  while (!p2_ans.match(PATT_INPUT)) { p2_ans = prompt("*** SECRET ***\nEnter Player 2 number (4 digits)") || ""; }
+
+  // guess number
+  if (next_turn == PLAYER_1) {
+    while (!input_num.match(PATT_INPUT)) { input_num = prompt("TURN: Player 1\nWhat is Player 2 number ?") || ""; }
+    p1_num.unshift(input_num);
+  }
+  else { // PLAYER_2
+    while (!input_num.match(PATT_INPUT)) { input_num = prompt("TURN: Player 2\nWhat is Player 1 number ?") || ""; }
+    p2_num.unshift(input_num);
+  }
+
+  // render table
+  let gameover = render_table([ p1_ans, p2_ans ], [ p1_num, p2_num ]);
+
+  // next turn
+  if (gameover) {
+    // game over
+    $('.btn-play').hide();
+    let player_label = next_turn ? "Player 1" : "Player 2";
+    alert(`🎉🎉🎉 ${player_label} WIN 🎉🎉🎉`);
+  }
+  else {
+    // switch player
+    input_num = "";
+    next_turn = !next_turn;
+  }
+}
+
+//
+// BUTTONS
+//
+
+$('.btn-play').click(engine);
+$('.btn-restart').click(_ => location.reload());
 
 //
 // UI
 //
 
 function render_table(answers, numbers) {
-  // circuit break
-  if(numbers[0].length == 0) {
-    $('.stadium').hide();
-    return;
-  }
   let [a1, a2] = answers;
   // loading
   let loading = `<tr><td colspan='4'>Loading...</td></tr>`;
@@ -39,14 +83,16 @@ function render_table(answers, numbers) {
     html += `
       <tr>
         <td>${n1}</td>
-        <td>${mark(a1, n1)}</td>
+        <td>${mark(a2, n1)}</td>
         <td>${n2}</td>
-        <td>${mark(a2, n2)}</td>
+        <td>${mark(a1, n2)}</td>
       </tr>
     `;
   });
   // render table
   $('table tbody').html(html);
+  // gameover flag
+  return html.indexOf(HINT_FULL.repeat(4)) > -1;
 }
 
 //
@@ -90,15 +136,16 @@ function mark(ans, num) {
   let count_else = 4 - count_match - count_found;
 
   // return
-  return 'O'.repeat(count_match)
-          + 'X'.repeat(count_found)
-          + '-'.repeat(count_else);
+  return HINT_FULL.repeat(count_match)
+         + HINT_HALF.repeat(count_found)
+         + HINT_NONE.repeat(count_else);
 }
 
 //
 // DEV
 //
 
+/*
 function rand_num () { // 4 digits
   return Math.floor(1000 + Math.random() * 9000);
 }
@@ -113,3 +160,4 @@ let numbers = [
 ];
 render_table(answers, numbers);
 $('h1').html(answers.join(","));
+*/
